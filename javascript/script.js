918301372bd11bf641e1554a25dcfd5f02b5b5d5
MatteoGiuntella <matteo.giuntella@virgilio.it>
2023-12-13 17:34:30 +0100

    bonus fatto

--- javascript/script.js
+++ javascript/script.js
@@ -10,24 +10,79 @@
 
 // 3- quando l'utente clicca sulla cella cambia colore e stampa il numero in console
 
+let start = false;
+
 let newGame = document.getElementById('newgame');
 newGame .addEventListener('click', function(){
 
-    for (let i = 1; i <= 100; i++) {
+    let difficult = document.getElementById('select').value;
+    console.log('difficult',difficult, typeof difficult)
+    let option = 49;
+    let classe = 'easy';
 
-        let cell = document.createElement('div');
-        cell.append(i);
-        cell.classList.add('cell')
-        document.getElementById("my-container-cell").append(cell); 
-
-        cell.addEventListener('click', function(){
-        
-            cell.classList.add('cell-active')
-
-        })  
+    if ( difficult == 'hard'){
+        option = 100
+        classe = 'hard';
     }
+
+    else if ( difficult == 'normal'){
+        option = 81
+        classe = 'normal';
+    }
+
+    if ( start == false ){
+        start = true; 
+        for (let i = 1; i <= option; i++) {
+
+            let cell = document.createElement('div');
+            cell.append(i);
+            cell.classList.add('cell', classe)
+            document.getElementById("my-container-cell").append(cell); 
+    
+            cell.addEventListener('click', function(){
+                cell.classList.add('cell-active')    
+            })  
+        }
+    }
+    else{
+        start = false;
+        document.getElementById("my-container-cell").innerHTML = '';
+    }  
 })
 
 function numberCell(min, max){
     return Math.floor(Math.random() * (max - min + 1) + 1);
 }
+
+// se il livello di difficoltà selezionato sarà medium le celle sarrano 81
+
+// se il livello di difficoltà selezionato sarà easy le celle saranno 49
+
+// se il livelli di diffcoltà selezionato sarà easy le celle saranno 100 _____
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
