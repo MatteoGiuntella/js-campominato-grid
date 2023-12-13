c725dca2a27ce1deda97a4cf105676cd3477794d
MatteoGiuntella <matteo.giuntella@virgilio.it>
2023-12-13 15:12:28 +0100

    progetto iniziato

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+// 1- creo un bottone che all'evento click farà apparire la tavola di gioco
+
+// 2- la tavola di gioco sarà composta da 10 caselle su 10 righe
+
+// 3- quando l'utente clicca sulla cella cambia colore e stampa il numero in console
